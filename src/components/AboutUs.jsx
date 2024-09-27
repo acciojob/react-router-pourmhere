285164ea1099e76d8aeaaad098ab749bcbc52545
POURMHERE <pourmhere@gmail.com>
2024-09-27 18:52:49 +0530

    ddd

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <div>
+      {/* <p>This application demonstrates the basic routing demo!</p>
+      <h2> 'This is a sample React Router program.' */}
+      <h2> This is a sample React Router program.</h2>
+    </div>
+  )
+}
+
+export default AboutUs
--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,36 @@
 
 import React from "react";
 import './../styles/App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from "./Home";
+import AboutUs from "./AboutUs";
+
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+        <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">AboutUs</Link>
+              </li>
+            </ul>
+          </nav>
+
+           <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<AboutUs />} />
+          </Routes>
+        </main>
+
+        </div>
+        </Router>
+    
   )
 }
 
